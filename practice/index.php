c53f1144dbeaefc80ee1abb93694bdb4316ab182
Murthy Garimella <mgarimella@g.harvard.edu>
2022-02-02 22:45:21 +0000

    updated with author info and practice app

---
+++ practice/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Practice App for e15</title>
+    <meta charset="utf-8">
+    <link href=data: , rel=icon>
+</head>
+
+<body>
+    <h1> Practice Application e-15</h1>
+</body>
+
+</html>
